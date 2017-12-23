8c1f2cb02fe8dd125e9867fa7c0fe5914420b7fa
LeeChunHei <lch19980829@gmail.com>
2017-12-23 15:53:26 +0800

    ftm updated

--- inc/libbase/ftm.h
+++ inc/libbase/ftm.h
@@ -5,16 +5,33 @@
  * Copyright (c) 2014-2017 HKUST SmartCar Team
  * Refer to LICENSE for details
  */
-
 #pragma once
+#include "libbase/pin.h"
 
 namespace libbase {
 
 class Ftm {
 public:
+	enum struct FTMn {
+		kFTM0 = 0, kFTM1, kFTM2 = 2
+	};
+
+	enum struct EXT_CLK {
+		kTCK_1 = 0, kTCK_2, kDisable = 3
+	};
+	
+	enum struct CHANNEL {
+		kCh0 = 0, kCh1, kCh2, kCh3, kDisable = 4
+	};
+
+	Ftm(FTMn ftmn, EXT_CLK external_clk);
+	~Ftm();
+	void InitChannel(CHANNEL ch);
 
 private:
-
+	const FTMn ftmn;
+	const EXT_CLK external_clk;
+	uint8_t opened_channel;
 };
 
 }
--- src/libbase/ftm.cpp
+++ src/libbase/ftm.cpp
@@ -6,7 +6,28 @@
  * Refer to LICENSE for details
  */
 
+#include <assert.h>
 #include "libbase/ftm.h"
+#include "libbase/cmsis/SKEAZ1284.h"
 
+namespace libbase {
 
+Ftm::Ftm(FTMn ftmn, EXT_CLK external_clk) :
+		ftmn(ftmn), external_clk(external_clk) {
+	opened_channel = 0;
+	SIM->SCGC |= 1 << (SIM_SCGC_FTM0_SHIFT + ftmn);
 
+}
+
+Ftm::~Ftm() {
+
+}
+
+void Ftm::InitChannel(CHANNEL ch) {
+	if ((opened_channel & (1 << ch)) || ch == CHANNEL::kDisable)	//return if the channel opened before or it is kDisable
+		return;
+	assert(ftmn < FTMn::kFTM2);
+
+}
+
+}
